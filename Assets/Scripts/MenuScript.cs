24294bb51089c652f2287341be916a116cedc635
יובל בן שיטרית <yuval056@gmail.com>
2025-05-13 21:21:38 +0300

    first commit

--- Assets/Scripts/MenuScript.cs
+++ Assets/Scripts/MenuScript.cs
@@ -40,4 +40,6 @@ public class MenuScript : MonoBehaviour
         Application.Quit();
     }
 
+  
+
 }
---
+++ Assets/Scripts/ZombieMovementScript.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class ZombieMovementScript : MonoBehaviour
+{
+    public float speed = 2f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    }
+}
